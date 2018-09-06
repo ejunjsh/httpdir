8a933192a8f96be3bb99e0a6fa1518e7a0712153
ejunjsh <junjie.shao@ericsson.com>
2018-09-06 14:40:50 +0800

    initial implement function

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # httpdir
 
 a commandline to make your directory become a static server
+
+## install
+
+    #  go get github.com/ejunjsh/httpdir
+
+## usage
+
+    # httpdir -help
+    Usage of httpdir:
+      -addr string
+            listen address (default ":6789")
+      -dir string
+            directory (default ".")
+      -help
+            usage
+
+## example
+
+    # httpdir
+
+then open browser and navigate to `http://localhost:6789`, you will see your files in the page.
--- main.go
+++ main.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"flag"
-	"net/http"
-	"log"
 	"fmt"
+	"log"
+	"net/http"
 )
 
-func main()  {
-	addr:=flag.String("addr",":6789","listen address")
-	dir:=flag.String("dir",".","directory")
-	help:=flag.Bool("help",false,"usage")
+func main() {
+	addr := flag.String("addr", ":6789", "listen address")
+	dir := flag.String("dir", ".", "directory")
+	help := flag.Bool("help", false, "usage")
 
 	flag.Parse()
 
-	if *help{
+	if *help {
 		flag.Usage()
+		return
 	}
 
-	h := http.FileServer(http.Dir(*dir))
-	fmt.Println("listening on "+*addr)
+	h := FileServer(http.Dir(*dir))
+	fmt.Println("listening on " + *addr)
 	err := http.ListenAndServe(*addr, h)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
